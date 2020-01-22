4c429e1ae752e29541bbfe5fafdc3e2bdfd5aad3
john <john@spinningplates.org>
2020-01-21 16:34:58 -0800

    added Cors dependancy

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const postsRouter = require('./router/posts-router');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
